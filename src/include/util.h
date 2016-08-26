9ab2b4fc9777d5051464903c17fd953c62558074
conqlima <robson.conq@gmail.com>
2016-08-26 18:36:15 -0400

    inclusão do Makefile e correção da função print

--- src/include/util.h
+++ src/include/util.h
@@ -4,5 +4,6 @@
 /*Protótipos das funções*/
 void control ( double x0, double y0, int m, double b, char* s, char* nome );
 void scrip_gnuplot ( char str[] );
-static void print(double x, ...);
+//static void print(long double x);
+//static void print(double x, ...);
 #endif
--- src/util.c
+++ src/util.c
@@ -18,7 +18,7 @@ void control ( double x0, double y0, int m, double b, char* s, char* nome )
     //unsigned long long j = 0.0;
     long double xn = x0;
     double h = ( b - x0 ) /m;
-    long double p = 0.0;
+    //long double p = 0.0;
     long double yv[4] = {y0,y0,y0,y0};    
     long double yx,y_e, y_em, y_rk3, y_rk4, y_dp, y_abm4;
     y_e = y_em = y_rk3 = y_rk4 = y_dp = y_abm4 = y0;    
@@ -31,21 +31,21 @@ void control ( double x0, double y0, int m, double b, char* s, char* nome )
     //fq = fopen ( "erro", "w" );
     for ( i = 0; xn < b+h ; xn = xn + h,i++ )
     {
-        //print(0,xn,p);
-        fprintf(fp, "%.4Le \t ",xn);
+        print(xn);
+        //fprintf(fp, "%.4Le \t ",xn);
         
         //print(xn,y_e,y_em,y_rk3,y_rk4,y_dp,y_abm4,yx,p);
         //fprintf ( fp, "%f \t %.30Le \t %.30Le \t %.30Le \t %.30Le \t %.30Le \t %.30Le \t %.30Le \n", xn, y_e, y_em, y_rk3, y_rk4, y_dp, y_abm4, yx);
         //fprintf ( fp, "%f \t %.4Le \t %.4Le \t %.4Le \t %.4Le \t %.4Le \t %.4Le \t %.4Le \n", xn, y_e, y_em, y_rk3, y_rk4, y_dp, y_abm4, y ( xn ) );
         //fprintf ( fq, "%f \t %.20Le \t %.20Le \t %.20Le \t %.20Le \t %.20Le \t %.20Le \t %.20Le \n", xn,  fabsl ( ( yx - y_e ) ), fabsl ( ( yx - y_em ) ), fabsl ( ( yx - y_rk3 ) ), fabsl ( ( yx - y_rk4 ) ), fabsl ( ( yx - y_dp ) ), fabsl ( ( yx - y_abm4 ) ),fabsl ( ( yx - yx ) ) );
-        strstr ( s,"e" ) != NULL ? print(0,y_e,p),( y_e = euler ( xn, y_e, h ) ) : ( y_e = 0 ) ;
-        strstr ( s,"rk2" ) != NULL ? print(0,y_em,p),( y_em = euler_modificado ( xn, y_em, h ) ) : ( y_em = 0 ) ;
-        strstr ( s,"rk3" ) != NULL ? print(0,y_rk3,p),( y_rk3 = runge_kutta_3 ( xn, y_rk3, h ) ) : ( y_rk3 = 0 ) ;
-        strstr ( s,"rk4" ) != NULL ? print(0,y_rk4,p),( y_rk4 = runge_kutta_4 ( xn, y_rk4, h ) ) : ( y_rk4 = 0 ) ;
-        strstr ( s,"dp" ) != NULL ? print(0,y_dp,p),( y_dp = dopri ( xn, y_dp, h ) ) : ( y_dp = 0 ) ;
+        strstr ( s,"e" ) != NULL ? print(y_e),( y_e = euler ( xn, y_e, h ) ) : ( y_e = 0 ) ;
+        strstr ( s,"rk2" ) != NULL ? print(y_em),( y_em = euler_modificado ( xn, y_em, h ) ) : ( y_em = 0 ) ;
+        strstr ( s,"rk3" ) != NULL ? print(y_rk3),( y_rk3 = runge_kutta_3 ( xn, y_rk3, h ) ) : ( y_rk3 = 0 ) ;
+        strstr ( s,"rk4" ) != NULL ? print(y_rk4),( y_rk4 = runge_kutta_4 ( xn, y_rk4, h ) ) : ( y_rk4 = 0 ) ;
+        strstr ( s,"dp" ) != NULL ? print(y_dp),( y_dp = dopri ( xn, y_dp, h ) ) : ( y_dp = 0 ) ;
         if ( strstr ( s,"abm4" ) != NULL )
         {
-			print(0.0,y_abm4,p);
+			print(y_abm4);
             if ( xn < 4*h+x0 )
             {
                 y_abm4 = dopri ( xn, y_abm4, h );
@@ -64,7 +64,7 @@ void control ( double x0, double y0, int m, double b, char* s, char* nome )
         else
         y_abm4 = 0;
         yx = y ( xn ); 
-        print(0,yx,p);
+        print(yx);
         fprintf(fp,"\n");
     }
     (void)fclose ( fp );
@@ -88,20 +88,36 @@ void control ( double x0, double y0, int m, double b, char* s, char* nome )
     //fprintf ( fp, "pause -1 \"Continuar?\" " );
     //fclose ( fp );
 //}
-static void print(double x, ...)
+static void print(long double y)
 {
-	long double y;
-	va_list argumentos;
-	va_start (argumentos,x);	
+	//long double y;
+	//va_list argumentos;
+	//va_start (argumentos,x);	
 	//fp = fopen ( "robson", "w" );
     //fq = fopen ( "erro", "w" );
 	//if (x == 0)
 	//fprintf(fp,"%f\t",x);
-	while((y = va_arg(argumentos,long double)) != 0.0)
+	//while((y = va_arg(argumentos,long double)) != 0.0)
 	fprintf(fp, "%.4Le \t ",y);
 	//fprintf(fp,"\n");
-	va_end(argumentos);
+	//va_end(argumentos);
 	//fclose ( fp );
-    //fclose ( fq );
-	
+    //fclose ( fq );	
 }
+
+//static void print(double x, ...)
+//{
+	//long double y;
+	//va_list argumentos;
+	//va_start (argumentos,x);	
+	////fp = fopen ( "robson", "w" );
+    ////fq = fopen ( "erro", "w" );
+	////if (x == 0)
+	////fprintf(fp,"%f\t",x);
+	//while((y = va_arg(argumentos,long double)) != 0.0)
+	//fprintf(fp, "%.4Le \t ",y);
+	////fprintf(fp,"\n");
+	//va_end(argumentos);
+	////fclose ( fp );
+    ////fclose ( fq );	
+//}
